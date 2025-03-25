e832ae1840d0e74cd3996d5ca8591ee3bda5af97
raulguiii <raul.rodrigues@aluno.faculdadeimpacta.com.br>
2025-03-25 20:03:22 -0300

    fazendo rota de login pro index

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+@app.route("/")
+def login():
+    return render_template("login.html")
 
-if __name__ == '__main__':
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
+@app.route("/login", methods=["POST"])
+def do_login():
+    return redirect(url_for("index"))
+
+if __name__ == "__main__":
     app.run(debug=True)
