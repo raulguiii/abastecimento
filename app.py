5fc29aa653708f5bbf84c0bfd5f5afa7b8295c66
raulguiii <raul.rodrigues@aluno.faculdadeimpacta.com.br>
2025-05-21 10:11:44 -0300

    teste do fim app.py

--- app.py
+++ app.py
@@ -772,4 +772,5 @@ def listar_abastecimentosVigilancia():
     return jsonify(abastecimentos)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
