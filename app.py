f21993aba40763db751be1dfa9d2c1ff9809596a
raulguiii <raul.rodrigues@aluno.faculdadeimpacta.com.br>
2025-05-21 10:51:19 -0300

    testes3

--- app.py
+++ app.py
@@ -771,6 +771,3 @@ def listar_abastecimentosVigilancia():
 
     return jsonify(abastecimentos)
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
