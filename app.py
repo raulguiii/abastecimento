622e1c335878b78276af6562e22a40d8e340f928
raulguiii <raul.rodrigues@aluno.faculdadeimpacta.com.br>
2025-05-21 14:48:35 -0300

    acho que agora vai

--- app.py
+++ app.py
@@ -771,5 +771,7 @@ def listar_abastecimentosVigilancia():
 
     return jsonify(abastecimentos)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+import os
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port, debug=True)
